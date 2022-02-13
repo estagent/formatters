19ea954167532ffc1b4b34c092e297bb05621d05
estagent <estagent@gmail.com>
2022-02-14 02:31:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# string time currency formatters
+
+
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import Preference from '@revgaming/preference'
+
+export default {
+  amount(amount, decimals = 2) {
+    return amount.toLocaleString(Preference.locale(), {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: decimals,
+    })
+  },
+  currencyAmount(currency_code, amount, decimals = 2) {
+    return amount.toLocaleString(Preference.locale(), {
+      style: 'currency',
+      currency: currency_code.toUpperCase(),
+      minimumFractionDigits: 0,
+      maximumFractionDigits: decimals,
+    })
+  },
+  formatDate: date => {
+    if (!date) return '-'
+    if (!(date instanceof Date)) date = new Date(date)
+    return date.toLocaleString(Preference.locale(), {
+      day: '2-digit', // numeric, 2-digit
+      year: 'numeric', // numeric, 2-digit
+      month: '2-digit', // numeric, 2-digit, long, short, narrow
+      timeZone: Preference.timezone(),
+    })
+  },
+  formatTime: date => {
+    if (!date) return '-'
+    if (!(date instanceof Date)) date = new Date(date)
+    return date.toLocaleString(Preference.locale(), {
+      day: '2-digit', // numeric, 2-digit
+      year: 'numeric', // numeric, 2-digit
+      month: '2-digit', // numeric, 2-digit, long, short, narrow
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      timeZone: Preference.timezone(),
+    })
+  },
+}
